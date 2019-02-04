481d377b6a36b8efea785d8c13b39a3c9c93c2ea
Marcella <marcella@Marcellas-MacBook-Air.local>
2019-02-04 17:20:14 -0500

    load data modal default to hidden

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import KeplerGl from 'kepler.gl';
+import { toggleModal } from 'kepler.gl/actions';
+import { toggleSidePanel } from 'kepler.gl/actions';
+
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
 class App extends Component {
+
+  componentDidMount() {
+  // load data modal default to hidden
+    this.props.dispatch(toggleModal(null))
+  }
+
   render() {
-    return (
-      <div className="App">
+return (
+      <div className="App" placeholder="false">
         <KeplerGl
           id="map"
           mapboxApiAccessToken={MAPBOX_TOKEN}
