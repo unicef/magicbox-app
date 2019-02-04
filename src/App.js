1e67a0f7b49426e1927c75152ab27360e481b975
Carlos Cardoso Dias <carlosdias.dev@gmail.com>
2019-02-04 21:01:38 -0200

    make app ocupy all screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import KeplerGl from 'kepler.gl';
+import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
+import './App.css';
 
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
@@ -9,11 +11,17 @@ class App extends Component {
 
   render() {
 return (
-      <div className="App" placeholder="false">
-        <KeplerGl
-          id="map"
-          mapboxApiAccessToken={MAPBOX_TOKEN}
-        />
+      <div className="App">
+        <AutoSizer>
+          {({height, width}) => (
+            <KeplerGl
+              id="map"
+              mapboxApiAccessToken={MAPBOX_TOKEN}
+              height={height}
+              width={width}
+            />
+          )}
+        </AutoSizer>
       </div>
     );
   }
