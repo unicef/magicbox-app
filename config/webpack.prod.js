7b460d1b5eb12a12c2f4ad021611d82aca5078ec
Carlos Cardoso Dias <carlosdias.dev@gmail.com>
2019-02-19 20:51:38 -0300

    add initial setup for webpack workbox plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -6,6 +6,8 @@ const common = require('./webpack.common');
 const CompressionPlugin = require('compression-webpack-plugin');
 // Compression algorithm
 const zopfli = require('@gfx/zopfli');
+// Workbox plugin to generate our SW
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 // Set NODE_ENV and BABEL_ENV to build scripts
 process.env.NODE_ENV = 'production';
@@ -14,6 +16,20 @@ process.env.BABEL_ENV = 'production';
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
+    // Generate SW
+    new WorkboxWebpackPlugin.GenerateSW({
+      clientsClaim: true,
+      exclude: [/\.map$/, /asset-manifest\.json$/],
+      importWorkboxFrom: 'cdn',
+      navigateFallback: '/index.html',
+      navigateFallbackBlacklist: [
+        // Exclude URLs starting with /_, as they're likely an API call
+        new RegExp('^/_'),
+        // Exclude URLs containing a dot, as they're likely a resource in
+        // public/ and not a SPA route
+        new RegExp('/[^/]+\\.[^/]+$'),
+      ],
+    }),
     // Compress assets
     new CompressionPlugin({
       compressionOptions: {
