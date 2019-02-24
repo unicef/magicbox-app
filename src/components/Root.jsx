70c91e377e1cba841a0b51a228d8418d85738445
Carlos Cardoso Dias <carlosdias.dev@gmail.com>
2019-02-24 12:53:00 -0300

    add initial app theme

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -3,17 +3,20 @@ import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import ConnectedApp from './App';
 
 // Root component
-const Root = ({ store, history }) => (
+const Root = ({ store, history, theme }) => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Switch>
-        <Route path="/c/:country/:dataset?" component={ConnectedApp} />
-        <Route path="/:dataset" component={ConnectedApp} />
-        <Route path="/" component={ConnectedApp} />
-      </Switch>
+      <MuiThemeProvider theme={theme}>
+        <Switch>
+          <Route path="/c/:country/:dataset?" component={ConnectedApp} />
+          <Route path="/:dataset" component={ConnectedApp} />
+          <Route path="/" component={ConnectedApp} />
+        </Switch>
+      </MuiThemeProvider>
     </ConnectedRouter>
   </Provider>
 );
@@ -21,6 +24,7 @@ const Root = ({ store, history }) => (
 Root.propTypes = {
   store: PropTypes.shape({}).isRequired,
   history: PropTypes.shape({}).isRequired,
+  theme: PropTypes.shape({}).isRequired,
 };
 
 export default Root;
