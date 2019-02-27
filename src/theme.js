2fbd2ada0f83e0212f3740d7036c331f5c947ed4
Carlos Cardoso Dias <carlosdias.dev@gmail.com>
2019-02-27 16:59:47 -0300

    add theme to application

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: '#000',
+    },
+    type: 'dark',
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
