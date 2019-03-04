7fcdd65273e955a59da634009f6a7241caa2e4b3
Marcella <marcellamaki@gmail.com>
2019-03-04 12:16:43 -0500

    hard code scale title and gradient bar

--- src/components/SidePanelScale/SidePanelScale.jsx
+++ src/components/SidePanelScale/SidePanelScale.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
 
 
 const styles = {
@@ -10,27 +11,32 @@ const styles = {
     margin: '0px',
     backgroundColor: '#e3e3e3',
   },
+  chip: {
+    marginLeft: '22px',
+    marginTop: '22px',
+    backgroundColor: 'gray',
+    width: '242px',
+    height: '10px',
+    borderRadius: '5px',
+    backgroundImage: 'linear-gradient(to right, #e4e6ea, #7280c7 44%, #182fab)',
+  },
+  title: {
+    paddingTop: '17px',
+    paddingLeft: '22px',
+    fontFamily: 'IBM Plex Sans',
+    fontSize: '13px',
+    fontWeight: 'bold',
+    letterSpacing: '0.9px',
+    color: '#000000',
+  },
 };
 
-class SidePanelScale extends Component {
-  constructor() {
-    super();
-    this.state = {
-      content: 'scale',
-    };
-  }
-
-  render() {
-    const { classes, title } = this.props;
-    const { content } = this.state;
-    return (
-      <div className={classes.scale}>
-        <h4>{title}</h4>
-        <h4>{content}</h4>
-      </div>
-    );
-  }
-}
+const SidePanelScale = ({ classes, title }) => (
+  <div className={classes.scale}>
+    <div className={classes.title}>{title}</div>
+    <Chip className={classes.chip} />
+  </div>
+);
 
 SidePanelScale.propTypes = {
   classes: PropTypes.shape({}).isRequired,
