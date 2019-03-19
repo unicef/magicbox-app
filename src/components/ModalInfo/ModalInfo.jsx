0b9b2d5662dab5e3a7240a5f21a0e94af789a497
Marcella <marcellamaki@gmail.com>
2019-03-19 14:33:02 -0400

    update to 3 categories of text: header, title, content

--- src/components/ModalInfo/ModalInfo.jsx
+++ src/components/ModalInfo/ModalInfo.jsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
-const ModalInfo = ({ content }) => (
+const styles = ({
+  header: {
+    marginLeft: '30px',
+    marginRight: '30px',
+    fontFamily: 'IBM Plex Sans',
+    fontSize: '24px',
+  },
+  title: {
+    marginLeft: '30px',
+    marginRight: '30px',
+    fontFamily: 'IBM Plex Sans',
+    fontSize: '22px',
+  },
+  text: {
+    marginLeft: '30px',
+    marginRight: '30px',
+    fontFamily: 'IBM Plex Sans',
+    fontSize: '16px',
+  },
+});
+
+const ModalInfo = ({ classes, content }) => (
   <span>
-    { content.content.map(c => <p>{c.title}</p>) }
+    { content.content.map(c => (
+      <span>
+        { c.header && <p className={classes.header}>{c.header}</p>}
+        { c.title && <p className={classes.title}>{c.title}</p>}
+        { c.content && <p className={classes.text}>{c.content}</p>}
+      </span>
+    ))
+    }
   </span>
 );
 
 ModalInfo.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
   content: PropTypes.shape({}).isRequired,
 };
 
-export default ModalInfo;
+export default withStyles(styles)(ModalInfo);
