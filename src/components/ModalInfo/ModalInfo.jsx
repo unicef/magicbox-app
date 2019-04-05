f628ef8d42d6385bfc3c897d81f46fafbe2f4720
Marcella <marcellamaki@gmail.com>
2019-04-05 12:17:46 -0400

    (WIP) add link capabilities for papers and talks in data modal

--- src/components/ModalInfo/ModalInfo.jsx
+++ src/components/ModalInfo/ModalInfo.jsx
@@ -4,22 +4,35 @@ import { withStyles } from '@material-ui/core/styles';
 
 const styles = ({
   header: {
-    marginLeft: '30px',
     marginRight: '57px',
     fontFamily: 'IBM Plex Sans',
     fontSize: '31px',
   },
   title: {
-    marginLeft: '30px',
     marginRight: '57px',
     fontFamily: 'IBM Plex Sans',
     fontSize: '22px',
   },
-  text: {
-    marginLeft: '30px',
+  titleLink: {
     marginRight: '57px',
     fontFamily: 'IBM Plex Sans',
     fontSize: '18px',
+    textDecoration: 'none',
+    color: '#000000',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+  text: {
+    marginRight: '57px',
+    fontFamily: 'IBM Plex Sans',
+    fontSize: '18px',
+  },
+  textGray: {
+    marginRight: '57px',
+    fontFamily: 'IBM Plex Sans',
+    fontSize: '18px',
+    color: '#828282',
   },
 });
 
@@ -27,9 +40,11 @@ const ModalInfo = ({ classes, content }) => (
   <span className={classes.body}>
     { content && content.content && content.content.map && content.content.map(c => (
       <span key={c.id}>
-        { c.header && <p className={classes.header}>{c.header}</p>}
-        { c.title && <p className={classes.title}>{c.title}</p>}
-        { c.content && <p className={classes.text}>{c.content}</p>}
+        { c.header && <p className={classes.header}>{c.header}</p> }
+        { c.titleLink && <p><a href={c.href} className={classes.titleLink}>{c.titleLink}</a></p> }
+        { c.title && <p className={classes.title}>{c.title}</p> }
+        { c.content && <p className={classes.text}>{c.content}</p> }
+        { c.contentGray && <p className={classes.textGray}>{c.contentGray}</p> }
       </span>
     ))
     }
