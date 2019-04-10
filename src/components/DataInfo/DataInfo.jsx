6285807371b0fed91787575ea0c583a90d2ef4e3
Carlos Cardoso Dias <carlosdias.dev@gmail.com>
2019-04-10 17:29:55 -0300

    add share and email icons

--- src/components/DataInfo/DataInfo.jsx
+++ src/components/DataInfo/DataInfo.jsx
@@ -5,6 +5,8 @@ import Drawer from '@material-ui/core/Drawer';
 import Fab from '@material-ui/core/Fab';
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
+import EmailIcon from '@material-ui/icons/Email';
+import ShareIcon from '@material-ui/icons/Share';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
@@ -30,12 +32,15 @@ const styles = theme => ({
     width: '570px',
     height: '90%',
   },
-  closeExploreButton: {
-    margin: theme.spacing.unit,
+  buttonsArea: {
     position: 'absolute',
     left: 0,
     top: 0,
-    transform: `translateX(-100%) translate(-${2 * theme.spacing.unit}px)`,
+    width: '72px',
+    transform: 'translateX(-100%)',
+  },
+  button: {
+    margin: theme.spacing.unit,
   },
   openExploreButton: {
     margin: theme.spacing.unit,
@@ -102,14 +107,33 @@ class DataInfo extends Component {
           classes={{ paper: classes.paper }}
           ModalProps={{ onBackdropClick: toggleAction }}
         >
-          <Fab
-            color="secondary"
-            aria-label="Close"
-            className={classes.closeExploreButton}
-            onClick={toggleAction}
-          >
-            <CloseIcon />
-          </Fab>
+          <div className={classes.buttonsArea}>
+            <Fab
+              color="secondary"
+              aria-label="Close"
+              className={classes.button}
+              onClick={toggleAction}
+            >
+              <CloseIcon />
+            </Fab>
+            <Fab
+              aria-label="E-mail"
+              className={classes.button}
+            >
+              <a
+                href="mailto:contact@magicbox.org"
+                style={{ color: 'inherit', display: 'contents' }}
+              >
+                <EmailIcon />
+              </a>
+            </Fab>
+            <Fab
+              aria-label="Share"
+              className={classes.button}
+            >
+              <ShareIcon />
+            </Fab>
+          </div>
           <div className={classes.dataInfoContent}>
             <div className={classes.dataInfoContent__tabs}>
               {content.map((item, i) => (
